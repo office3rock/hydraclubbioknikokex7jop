d7251f5c0f7dd006a7db80e4e565bac64ad5c9a4
office3rock <105702924+office3rock@users.noreply.github.com>
2024-01-13 09:27:40 +0200

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -18,26 +18,26 @@ module.exports = {
   subscribeWidget: {
     visible: false,
     title: "hydraclubbioknikokex7jop.com",
-    helpText: "הישארו מעודכנים",
-    successMessage: "תודה ההודעה נשלחה בהצלחה",
+    helpText: "Keep following",
+    successMessage: "Thank you The message was sent successfully",
   },
   header: {
     navigation: [
       {
-        label: "דף הבית",
+        label: "Homepage",
         url: "https://hydraclubbioknikokex7jop.com/",
       },
       {
-        label: "יצירת קשר",
+        label: "Contact",
         url: "https://hydraclubbioknikokex7jop.com/contact",
       },
     ],
   },
   footer: {
-    copyright: "כל הזכויות שמורות ",
+    copyright: "All rights reserved",
     navigation: [
       {
-        label: "דף הבית",
+        label: "Homepage",
         url: "https://hydraclubbioknikokex7jop.com/",
       },
       {
@@ -46,7 +46,7 @@ module.exports = {
       },
       
       {
-        label: "יצירת קשר",
+        label: "Contact",
         url: "https://hydraclubbioknikokex7jop.com/contact",
       },
      
